c3b2f1a4f0c059ea6e9bae26f0fca17b11c9c8d0
Jose Felipe <felipe.brx.dev@gmail.com>
2024-01-20 12:06:07 -0300

    upgrade app e add news requiriments

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import math
 
 from arquivos.player import Player
 from arquivos.gui import GuiAplication
+from tkinter.filedialog import askdirectory
 
 
 class Aplication(GuiAplication, Player):
@@ -9,10 +10,7 @@ class Aplication(GuiAplication, Player):
         super().__init__(**kwargs)
         self.buscar_arquivos_mp3()
         self.iniciar()
-        self.tela_principal()
-        self.frames_tela()
-        self.widgets_controles()
-        self.widgets_barra_progresso()
+        self.build_interface()
         self.config_command()
         self.root.protocol("WM_DELETE_WINDOW", self.evento_close)
         self.root.after(0, self.update_bar_progress)
@@ -36,8 +34,15 @@ class Aplication(GuiAplication, Player):
         self.bt_play_pause.configure(text=self.acao)
 
     def button_atualizar_playlist(self):
-        self.buscar_arquivos_mp3()
+        self.pasta_musicas = askdirectory()
+        self.buscar_arquivos_mp3(self.pasta_musicas)
         self.iniciar()
+        print('len(self.playlist) ', len(self.playlist))
+        if len(self.playlist) < 1:
+            print('re cc')
+            self.label_musica.configure(text=f"--- Sem músicas na lista de reprodução ---")
+            self.label_init.configure(text=f'00:00')
+            self.label_end.configure(text=f'00:00')
 
     def button_avancar(self):
         self.avancar_musica()
@@ -51,12 +56,13 @@ class Aplication(GuiAplication, Player):
         self.set_volume(float(format(self.bar_volume.get() / 100, '.2f')))
 
     def update_music_infor(self):
-        duration_counter = math.trunc(float(self.duration_music))
+        duration_counter = math.trunc(float(self.data_musica['duration']))
         duration_minutes = int(duration_counter) // 60
         duration_seconds = int(duration_counter) % 60
 
         str_seconds = f"0{duration_seconds}" if duration_seconds < 10 else f"{duration_seconds}"
         self.label_end.configure(text=f'{duration_minutes}:{str_seconds}')
+        self.label_musica.configure(text=f"musica - {self.data_musica['nome']}")
 
     def update_bar_progress(self):
         counter = math.trunc(float(self.get_pos_music()))
